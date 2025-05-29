69fd6b8d1c59e9c7233993c415d4d9bbc56795e5
bludzetelksina <bludzetelksina@gmail.com>
2025-05-29 14:48:03 +0300

    Update download-file.sh

--- config/download-file.sh
+++ config/download-file.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 # Использование: ./download-file.sh https://example.com/file.iso /disk/file.iso
 
-URL="$1"
-OUTPUT="$2"
+VERSION="$1"
+DOWNLOAD="$2"
 
-# Проверка параметров
-if [[ -z "$URL" || -z "$OUTPUT" ]]; then
-  echo "Использование: $0 <url> <путь_сохранения>"
-  exit 1
-fi
-
-# Скачиваем файл
-curl -L "$URL" -o "$OUTPUT"
-
-# Проверка результата
-if [[ $? -eq 0 ]]; then
-  echo "Файл успешно загружен: $OUTPUT"
-else
-  echo "Ошибка загрузки файла"
-  exit 1
-fi
+wget "$VERSION" -O "$DOWNLOAD"

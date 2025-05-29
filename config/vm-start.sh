6fea073afc3f837a6b0a35fc105f4291eef779c4
bludzetelksina <bludzetelksina@gmail.com>
2025-05-29 16:23:15 +0300

    Update vm-start.sh

--- config/vm-start.sh
+++ config/vm-start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Запускаем noVNC и VM
 
+# Прокси VNC через WebSocket для браузера
 /websockify --web=/opt/noVNC 6080 localhost:5901 &
 
-# Графическое окружение
+# Запуск графической среды
 Xvfb :1 -screen 0 1024x768x16 &
+sleep 2
 DISPLAY=:1 fluxbox &
 
 # Запуск виртуальной машины
@@ -13,3 +15,9 @@ qemu-system-x86_64 \
   -hda /disk/vm-disk.img \
   -vnc :1 \
   -enable-kvm
+
+# Вывод URL
+echo "Открой в браузере: http://localhost:6080/vnc.html"
+
+# Ждём завершения фоновых процессов (опционально)
+wait

252ffab14c481b26a97abffc09a7e1e2748a4285
bludzetelksina <bludzetelksina@gmail.com>
2025-05-30 11:01:09 +0300

    Update create-disk.sh

--- config/create-disk.sh
+++ config/create-disk.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e -x
 
-# Define the path and size for the disk image
-DISK_IMAGE="/disk/vm-disk.img"
-DISK_SIZE="10G"
-
-# Create the disk image using qemu-img
-qemu-img create -f qcow2 "$DISK_IMAGE" "$DISK_SIZE"
+# пример содержимого
+qemu-img create -f qcow2 disk/vm-disk.img 10G
